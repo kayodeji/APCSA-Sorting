60bdd1e10c0c512ca58adcab7e165260e08cffdc
Kymberly Ayodeji <51680946+kayodeji@users.noreply.github.com>
2020-03-04 20:05:58 -0600

    reverted back to original with no static methods

--- SortInterface.java
+++ SortInterface.java
@@ -1,6 +1,6 @@
 public interface SortInterface
 {
-    public static void bubbleSort(int[] array, int lowindex, int highindex, boolean reversed);
-    public static void selectionSort(int[] array, int lowindex, int highindex, boolean reversed);
-    public static void mergeSort(int[] array, int lowindex, int highindex, boolean reversed);
+    public void bubbleSort(int[] array, int lowindex, int highindex, boolean reversed);
+    public void selectionSort(int[] array, int lowindex, int highindex, boolean reversed);
+    public void mergeSort(int[] array, int lowindex, int highindex, boolean reversed);
 }
